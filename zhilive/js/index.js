cfbcf0b566ff40095795991c79143413ab552c8d
离心力 <lixinli0327@gmail.com>
2022-05-14 16:27:43 +0800

    fix bug

--- zhilive/js/index.js
+++ zhilive/js/index.js
@@ -1,17 +1,2 @@
-function compareDate(beginDateStr, endDateStr) {
-    var curDate = new Date(),
-        beginDate = new Date(beginDateStr),
-        endDate = new Date(endDateStr);
-    if (curDate >= beginDate && curDate <= endDate) {
-        return true;
-    }
-    return false;
-}
 
-let contents = document.getElementsByClassName('zj_liveInfo_info_p2');
-let aaa =document.getElementById("aaaaa");
-if(compareDate('2022/05/12 23:05', '2022/05/12 23:59')){
-  console.log('直播进行中');
-  console.log(contents);
-  console.log(aaa);
-}
+
